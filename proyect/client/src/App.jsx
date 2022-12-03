f83f08683a6bee8ce7ee8368ec0f354d678e15f2
John Villamil <jdsvg997@outllok.com>
2022-12-03 16:55:32 -0500

    Creando componentes base. Agregando CSS
    Se crearon los componentes base: Navbar, Loader, Welcome, Services, Transactions, Footer. Y se agrego codigo de CSS para manejar los estilos de la App

--- proyect/client/src/App.jsx
+++ proyect/client/src/App.jsx
@@ -1,54 +1,25 @@
 import { useState } from 'react'
+
 import reactLogo from './assets/react.svg'
+
+import { Navbar, Loader, Welcome, Services, Transactions, Footer } from './components/index'
+
 import './App.css'
 
-const  App = () => {
-  const [count, setCount] = useState(0)
+const App = () => {
 
   return (
-    <div className="App">
+    <div className="min-h-screen">
 
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='gradient-bg-welcome'>
+      <Navbar/>
+      <Loader/>
+      <Welcome/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 2)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
+      <Services/>
+      <Transactions/>
+      <Footer/>
+
     </div>
   )
 }
