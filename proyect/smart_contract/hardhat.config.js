241ad454ccda7f16abc3b6db33660a3660dabc7d
John Villamil <jdsvg997@outlook.com>
2023-01-15 16:33:58 -0500

    Configurando HardHat. Tipeado el desplegador del contrato. Correcciones minimas
    Se configuro hardhat.config.js, para la gestion de las transferencia necesaria (consumo de gas) para la compilacion del contrato parametrizando tambien el portal por el cual se haria la transferencia. Se tipeo el desplegador del contrato: deploy.js el cual compilara el contrato inteligente. Se realizaron correctacciones minimas sobre Transactions.sol

--- proyect/smart_contract/hardhat.config.js
+++ proyect/smart_contract/hardhat.config.js
@@ -1,6 +1,14 @@
-require("@nomicfoundation/hardhat-toolbox");
+// https://eth-goerli.g.alchemy.com/v2/usDhfIX_-S69NH9sI7Mjj9WN2iIXbOBV
+require('@nomiclabs/hardhat-waffle');
+// require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.10",
+  networks:{
+    ropsten:{
+      url: 'https://eth-goerli.g.alchemy.com/v2/usDhfIX_-S69NH9sI7Mjj9WN2iIXbOBV',
+      accounts: [ 'f86c6c46f708e6e4ceab3adf785643c2129487a414bf1fa1a8eb2687f776721f' ]
+    }
+  }
 };
