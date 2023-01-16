162d37fe258586ce5e57f2a1c91b2dcb415434eb
John Villamil <jdsvg997@outlook.com>
2023-01-16 06:33:49 -0500

    Creacion de la API-Point. Encapsulando contexto de la App en la API-Point. Componente Welcome setea la API-Point
    Se realiza la creacion de una API-Point (TransactionContex) la cual servira de conexion entre la AppWeb y el ambiente crypto con MetaMask, se entiende a la API-Point como un 'contexto' de la AppWeb. Se encapsula la AppWeb en el contexto de la API-Point. Se realizan modificaciones al componente Welcome, ya que este es el que esta orientado al seteo de la API-Point para el envio de transacciones

--- proyect/client/src/components/Welcome.jsx
+++ proyect/client/src/components/Welcome.jsx
@@ -1,34 +1,39 @@
+import React, { useContext } from 'react';
 import { AiFillPlayCircle } from "react-icons/ai";
 import { SiEthereum } from 'react-icons/si';
 import { BsInfoCircle } from "react-icons/bs";
 
+import { TransactionContext } from "../context/TransactionContext";
 import { Loader } from './'
 
 const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
-const Input = ({placeholder, name, type, value, handleChange}) => (
+const Input = ({ placeholder, name, type, value, handleChange }) => (
     <input
-    placeholder={placeholder}
-    name={name}
-    type={type}
-    step="0.00001"
-    value={value}
-    onChange={(e) => handleChange(e, name)}
-    className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
-    
+        placeholder={placeholder}
+        name={name}
+        type={type}
+        step="0.00001"
+        value={value}
+        onChange={(e) => handleChange(e, name)}
+        className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
+
     />
-    
+
 );
 
 const Welcome = () => {
 
-    const connectWallet = () => {
+    // ** useContext
+    const { connectWallet, currentAccount, formData, setFormData, handleChange, sendTransaction } = useContext(TransactionContext);
+    // ** useContext
 
-    }
-
-    const handleSubmit = () =>{
-         
-    }
+    const handleSubmit = (e) => {
+        const { addressTo, amount, keyword, message } = formData;     
+        e.preventDefault();   
+        if(!addressTo || !amount || !keyword || !message) return;
+        sendTransaction();
+    };
 
 
     return (
@@ -41,7 +46,8 @@ const Welcome = () => {
                     <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
                         Explore the Crypto world. Buy and sell cryptocurrencies easily on Krypto.
                     </p>
-                    <button
+                    {/* If currentAccount == true => !currentAccount == false => false && true => false :: Don't show the button */}
+                    {!currentAccount && (<button
                         type="button"
                         onClick={connectWallet}
                         className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd] "
@@ -49,7 +55,7 @@ const Welcome = () => {
                         <p className="text-white text-base font-semibold">
                             Connect Wallet
                         </p>
-                    </button>
+                    </button>)}
 
                     <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
                         <div className={`rounded-tl-2xl ${commonStyles}`}>Relativity</div>
@@ -105,24 +111,25 @@ const Welcome = () => {
                     </div>
 
                     <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
-                        <Input placeholder="Address To" name="addressTo" type="text" handleChange={() => {}}/>
-                        <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={() => {}}/>
-                        <Input placeholder="Keyword (Gif)" name="Keyword" type="text" handleChange={() => {}}/>
-                        <Input placeholder="Enter Message" name="message" type="text" handleChange={() => {}}/>
-                    
-                    <div className="h-[1px] w-full bg-gray-400 my-2"/>
-                    
-                    {/* {isLoading ? ( */}
-                    {true ? (
-                        <Loader />
-                    ) : (
-                        <button 
-                        type="button"
-                        onClick={handleSubmit}
-                        className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] rounded-full cursor-pointer">
-                            Send Now
-                        </button>
-                    )}
+                        <Input placeholder="Address To" name="addressTo" type="text" handleChange={handleChange} />
+                        <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={handleChange} />
+                        <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={handleChange} />
+                        <Input placeholder="Enter Message" name="message" type="text" handleChange={handleChange} />
+
+                        <div className="h-[1px] w-full bg-gray-400 my-2" />
+
+                        {/* Test */}
+                        {/* {false ? ( */}
+                        {false ? (
+                            <Loader />
+                        ) : (
+                            <button
+                                type="button"
+                                onClick={handleSubmit}
+                                className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] rounded-full cursor-pointer ">
+                                Send Now
+                            </button>
+                        )}
 
 
                     </div>
