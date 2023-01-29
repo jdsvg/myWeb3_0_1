b0df3ab7c61ada7e759e89523fa1c5a7c314cd78
John Villamil <jdsvg997@outlook.com>
2023-01-29 14:41:10 -0500

    dummyData, useFetch, Transactions, TransactionContext, Welcome, Footer
    dummyData: El presente documento nos servirá de auxiliar para mostrar algunas transacciones 'dummys'. useFetch: Se genera el siguiente modulo el cual nos ayudara a conectar la API de GIPHY, y asi llamar diferentes gifs en referencia a la Keyword. Transactions: Se genera el elemento dinámico 'TransactionCard', para que amolde cada uno de las transacciones realizadas junto con cada uno la informacion pertinente de cada transacción. Y en 'Transactions', se listan cada una de las transacciones. TransactionContext: se genera 'getAllTransactions' para obtener todas las transacciones de una cuenta. Para evitar o controlar los errores a la hora obtener las transacciones de una cuenta se genera también 'checkIfTransactionsExists', para obtener las diferentes transacciones de una cuenta en caso de que dicha cuenta tenga transacciones o controlar el error en caso de no tener transacciones. Welcome: Se importa 'shortenAddress', para mostrar la de forma cómoda la cuenta, y se llama 'isLoading', para complementar el control de espera a la acción de la ejecución de la transaccion. Footer: Se genera el ultimo modulo con el fin de mostrar informacion complementaria a la DappWeb

--- proyect/client/src/components/Welcome.jsx
+++ proyect/client/src/components/Welcome.jsx
@@ -5,6 +5,7 @@ import { BsInfoCircle } from "react-icons/bs";
 
 import { TransactionContext } from "../context/TransactionContext";
 import { Loader } from './'
+import { shortenAddress } from '../utils/shortenAddress';
 
 const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
@@ -25,7 +26,7 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
 const Welcome = () => {
 
     // ** useContext
-    const { connectWallet, currentAccount, formData, setFormData, handleChange, sendTransaction } = useContext(TransactionContext);
+    const { connectWallet, currentAccount, formData, setFormData, handleChange, sendTransaction, isLoading } = useContext(TransactionContext);
     // ** useContext
 
     const handleSubmit = (e) => {
@@ -101,7 +102,7 @@ const Welcome = () => {
                             </div>
                             <div >
                                 <p className="text-white font-light text-sm ">
-                                    0xasdasds....Y0ur4ddr3ss
+                                    {shortenAddress(currentAccount)}
                                 </p>
                                 <p className="text-white font-semibold text-lg mt-1">
                                     Ethereum
@@ -120,7 +121,9 @@ const Welcome = () => {
 
                         {/* Test */}
                         {/* {true ? ( */}
-                        {false ? (
+                        {/* Test */}
+                        {/* {false ? ( */}
+                        {isLoading ? (
                             <Loader />
                         ) : (
                             <button
